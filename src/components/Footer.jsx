d44a471b7931037d9e860b00a0e46592e0ea70e1
rachitsgh <rachit2351@gmail.com>
2024-07-09 14:42:39 +0530

    created Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,36 @@
 // Footer.jsx
 import React from 'react';
 import { SectionWrapper } from '../hoc';
+import { leetcode } from '../assets';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-black text-white py-8">
       <div className="container mx-auto text-center">
         <h1 className="text-2xl mb-6">Follow Us</h1>
-        <div className="flex justify-center space-x-6">
-          <a href="https://facebook.com" className="group">
-            <i className="fab fa-facebook-f text-3xl group-hover:text-blue-500"></i>
+        <div className="flex justify-center gap-8 space-x-6">
+          <a href="https://www.snapchat.com/add/rachitsnp.10?share_id=2ywlOGDSJtc&locale=en-US" className="group">
+            <i className="fab fa-snapchat text-3xl group-hover:text-yellow-200"></i>
           </a>
-          <a href="https://instagram.com" className="group">
+          <a href="https://www.instagram.com/rachitsig.10/" className="group">
             <i className="fab fa-instagram text-3xl group-hover:text-pink-500"></i>
           </a>
-          <a href="https://twitter.com" className="group">
-            <i className="fab fa-twitter text-3xl group-hover:text-blue-400"></i>
+          <a href="https://leetcode.com/u/tanzirou/" className="group">
+            <img
+              src={leetcode}
+              alt='leetcode' 
+
+              className='h-8 w-8 group-hover:color-yellow-500' 
+            />
           </a>
-          <a href="https://github.com" className="group">
+          <a href="https://github.com/rachitsgh" className="group">
             <i className="fab fa-github text-3xl group-hover:text-gray-300"></i>
           </a>
-          <a href="https://linkedin.com" className="group">
+          <a href="https://www.linkedin.com/in/rachit-pathak-977228236/" className="group">
             <i className="fab fa-linkedin-in text-3xl group-hover:text-blue-600"></i>
           </a>
         </div>
-        <p className="mt-6 text-gray-400">© 2024 Your Company. All rights reserved.</p>
+        <p className="mt-6 text-gray-400">© 2024 rachit.life . All rights reserved.</p>
       </div>
     </footer>
   );
